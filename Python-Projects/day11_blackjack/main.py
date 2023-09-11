1609583925af6d897f9c2bf5dde8a6c954bef581
Zackry Langford <zackrylangford@gmail.com>
2023-09-11 18:38:48 -0400

    initial refactor

--- Python-Projects/day11_blackjack/main.py
+++ Python-Projects/day11_blackjack/main.py
@@ -7,55 +7,51 @@ print(logo)
 play_game = input("Would you like to play a hand of black jack? (y/n)").lower()
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+player_hand = []
+dealer_hand = ["X"]
 
-def deal_card(hand):
+
+def deal_card(hand, num):
     """Deal a card to either the player or dealer"""
-    if hand == "player":
-        player_hand.append(random.choice(cards))
-    if hand == "dealer":
-        dealer_hand.append(random.choice(cards))
+    for x in range(num):
+        if hand == "player":
+            player_hand.append(random.choice(cards))
+        if hand == "dealer":
+            dealer_hand.append(random.choice(cards))
 
-while play_game == "y":
+def time_delay(num):
+    """Pause the game to make it more engaging"""
+    for x in range(num):
+        time.sleep(1)
+        print(".")
+
+def initial_deal():
+    """Initial Cards are dealt"""
     # Inital cards are dealt
-    print("Dealing cards to player...")
-    time.sleep(1)
-    print(".")
-    time.sleep(1)
-    print("..")
-
-    player_hand = []
-    deal_card("player")
-    deal_card("player")
-    print(f"Players hand: {player_hand}")
-
-    #Dealing cards to dealer
-    print("Dealing cards to dealer...")
-    time.sleep(1)
-    print(".")
-    time.sleep(1)
-    print("..")
-    dealer_hand = []
-    dealer_hand.append("X")
-    deal_card("dealer")
+    print(f"Dealing cards...")
+    time_delay(1)
+    deal_card("player", 2)
+    deal_card("dealer", 1)
     print(f"Dealers hand: {dealer_hand}")
-    print(f"Players hand: {player_hand}")
-
-    # Player is prompted to hit or stay
     player_total = sum(player_hand)
-
+    print(f"Players hand: {player_hand}, Total: {player_total}")
+    
+while play_game == "y":
+    #Deal the cards and set up the hand
+    initial_deal()
     player_stays = False
     player_busts = False
+    player_total = sum(player_hand)
 
     while not player_stays and not player_busts:
         if player_total == 21:
             print("Blackjack! Player automatically stands at 21")
             player_stays = True
         while player_total < 21 and player_stays == False:
-            print(f"Your total is {player_total}")
             if player_total < 21:
                 hit = input("Do you wish to hit or stay? (h for hit, s for stay)")
                 if hit == "h":
-                    deal_card("player")
+                    deal_card("player", 1)
                     player_total = sum(player_hand)
                     print(f"Players hand: {player_hand}\nPlayer total is {player_total}")
                     if player_total == 21:
@@ -79,13 +75,8 @@ while play_game == "y":
         while dealer_total < 17 and dealer_total < 21:
             print("Dealing another card to dealer...")
             #Sleep for 2 seconds and print a filler ellipses
-            time.sleep(1)
-            print(".")
-            time.sleep(1)
-            print("..")
-            time.sleep(1)
-            print("...")
-            deal_card("dealer")
+            time_delay(2)
+            deal_card("dealer", 1)
             print(dealer_hand)
             dealer_total = sum(dealer_hand)
             print(f"Dealer has: {dealer_total}")
