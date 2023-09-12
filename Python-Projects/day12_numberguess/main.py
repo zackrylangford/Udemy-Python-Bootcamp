bf4ce417cf3b118194edb3cad4833c7d5895e77e
Zackry Langford <zackrylangford@gmail.com>
2023-09-12 11:49:13 -0400

    basic game setup - user is guessing numbers

--- Python-Projects/day12_numberguess/main.py
+++ Python-Projects/day12_numberguess/main.py
@@ -1,14 +1,36 @@
 from art import logo
-
+import random
 print(logo)
 
+
+
 # User asked what difficulty level and setting the original guesses_remaining variable
+settings_chosen = False
 
-difficulty_level = input("What difficulty level would you like to play at? Easy or Hard\n").lower()
+while not settings_chosen:
+    difficulty_level = input("What difficulty level would you like to play at? Easy or Hard\n").lower()
+    if difficulty_level == "easy":
+        guesses_remaining = 10
+        settings_chosen = True
+    elif difficulty_level == "hard":
+        guesses_remaining = 5
+        settings_chosen = True
+    else:
+        print("Please select a difficulty.")
 
-if difficulty_level == "easy":
-    guesses_remaining = 10
-elif difficulty_level == "hard":
-    guesses_remaining = 5
+#Computer chooses a random number between 1-100
+cpu_choice = random.choice(range(101))
 
-print(guesses_remaining)
+#Debug statement - To be removed
+print(f"User guesses remaining: {guesses_remaining}")
+
+#Debug statement - To be removed
+print(f"CPU number chosen: {cpu_choice}")
+
+game_over = False
+
+while not game_over: 
+    guessed_number = int(input("Please guess a number between 1-100\n"))
+    print(f"You guessed: {guessed_number}")
+
+      
