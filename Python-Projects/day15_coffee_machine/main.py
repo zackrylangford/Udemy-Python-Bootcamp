e4c4de70609f8254100646f81b2f412c2c68b50f
Zackry Langford <zackrylangford@gmail.com>
2023-09-15 12:02:27 -0400

    money paid, need to check to make sure it is enough

--- Python-Projects/day15_coffee_machine/main.py
+++ Python-Projects/day15_coffee_machine/main.py
@@ -1,24 +1,62 @@
 # Coffee machine
-
+from menu import MENU, resources, resource_units
 # Initial resource values
 
+coffee_machine_on = True
 
-resources = [
-    {"water": 300, "type": "ml"},
-    {"milk": 200, 'type': "ml"},
-    {"coffee": 100, "type": "g"},
-    {"money": 0, "type": "$"},
-]
 
-user_order = input("What would you like? (espresso/latte/cappuccino): ")
+def check_resources(order):
+    """Check if there are enough resources to make the order"""
+    ingredients = MENU[order]["ingredients"]
 
-if user_order == "off":
-    print("Shutting down")
-elif user_order == "report":
-    for resource in resources:
-        for name, amount in resource.items():
-            if name != "type":
-                print(f"{name.capitalize()}: {amount}{resource['type']}")
-else:
-    print("Please select a valid option")
+    for item, amount in ingredients.items():
+        if item in resources:
+            if amount > resources[item]:
+                print(f"Sorry there is not enough {item}")
+                return False
+    return "Enough resources"
+
+
+def process_coins():
+    """Process the coins inserted by the user"""
+    print("Please insert coins.")
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickles = int(input("How many nickles?: "))
+    pennies = int(input("How many pennies?: "))
+
+    total = quarters * 0.25 + dimes * 0.10 + nickles * 0.05 + pennies * 0.01
+    return total
+
+
+def check_transaction(total, order):
+    pass
+
+def update_resources():
+    pass
+
+
+def report():
+    for name, amount in resources.items():
+        print(f"{name.capitalize()}: {amount}{resource_units[name]}")
+
+
+
+while coffee_machine_on:
+    user_order = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_order == "off":
+        print("Shutting down")
+        coffee_machine_on = False
+    elif user_order == "report":
+        report()
+    elif user_order in MENU:
+        print(f"You ordered: {user_order}")
+        if check_resources(user_order) == False:
+            report()
+        else:
+            money_paid = (process_coins())
+            print(money_paid)
+    else:
+        print("Please select a valid option")
+
 
