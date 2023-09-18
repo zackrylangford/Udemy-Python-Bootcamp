22eb4d25a288308b7a55862cf64effe4962cc5e0
Zackry Langford <zackrylangford@gmail.com>
2023-09-18 13:20:44 -0400

    completed the project

--- Python-Projects/day-16-Coffee-MachineOOP/main.py
+++ Python-Projects/day-16-Coffee-MachineOOP/main.py
@@ -5,22 +5,26 @@ from money_machine import MoneyMachine
 is_on = True
 zacks_machine = CoffeeMaker()
 zacks_menu = Menu()
-valid_order = ["espresso", "latte", "cappucino"]
+zacks_money = MoneyMachine()
 
 while is_on:
     order = input("What would you like to order? (espresso/latte/cappucino)")
     if order == "off":
         is_on = False
         print("Coffee machine shutting down")
-    if order == "report":
+    elif order == "report":
         zacks_machine.report()
-    elif order in valid_order:
+        zacks_money.report()
+    elif order in zacks_menu.get_items():
         user_order = zacks_menu.find_drink(order)
         if zacks_machine.is_resource_sufficient(user_order):
-            print("Yes")
+            order_cost = user_order.cost
+            print(f"A {order} costs: ${order_cost:.2f}")
+            if zacks_money.make_payment(order_cost):
+                zacks_machine.make_coffee(user_order)
 
     else:
-        print("not a valid order")
+        print("Sorry that item is not available")
 
 
 
