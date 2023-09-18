d528585bcb8e80f850f48814129cfd26127562a1
Zackry Langford <zackrylangford@gmail.com>
2023-09-18 13:49:12 -0400

    added a refactored version

--- Python-Projects/day-16-Coffee-MachineOOP/refactor.py
+++ Python-Projects/day-16-Coffee-MachineOOP/refactor.py
@@ -8,20 +8,18 @@ zacks_menu = menu.Menu()
 zacks_money = MoneyMachine()
 
 while is_on:
-    order = input("What would you like to order? (espresso/latte/cappuccino)")
+    options = zacks_menu.get_items()
+    order = input(f"What would you like to order?{options}")
     if order == "off":
         is_on = False
         print("Coffee machine shutting down")
     elif order == "report":
         zacks_machine.report()
         zacks_money.report()
-    elif order in zacks_menu.get_items():
+    else:
         user_order = zacks_menu.find_drink(order)
         if zacks_machine.is_resource_sufficient(user_order):
             order_cost = user_order.cost
             print(f"A {order} costs: ${order_cost:.2f}")
             if zacks_money.make_payment(order_cost):
                 zacks_machine.make_coffee(user_order)
-
-    else:
-        print("Sorry that item is not available")
